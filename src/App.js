cf40b6fa22e8156f7ed80cd837a8b3f220e3511d
Ziv <zivaflalo11@gmail.com>
2022-11-21 19:38:23 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cuonter from './Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      
+      <Cuonter/>
+    </div>
+    
+  );
+}
+
+export default App;
